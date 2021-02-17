0bb898af560f9dd4efbf1a8c271a206963b82fd7
krorina1013 <mee052@gmail.com>
2021-02-17 14:47:20 +0900

    commite

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -98,12 +98,12 @@ $(function () {
     .find("li")
     .click(function () {
       if (index == 1) {
-        index = 1;
+        index = 0;
         $(".message-wrap").find(".message").css({
           opacity: "1"
         });
       } else {
-        index = 1;
+        index = 0;
         $(".message-wrap").find(".message").css({
           opacity: "0"
         });
